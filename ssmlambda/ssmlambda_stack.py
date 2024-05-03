59c30c6719de4da44cc09017e8175d518312a86e
Chaya Singh <chaya.singh@flairminds.com>
2024-05-03 20:41:06 +0530

    SSM lambda

--- ssmlambda/ssmlambda_stack.py
+++ ssmlambda/ssmlambda_stack.py
@@ -1,17 +1,37 @@
 from aws_cdk import (
     # Duration,
+    aws_lambda as _lambda,
+
+    aws_ssm as ssm,
     Stack,
     # aws_sqs as sqs,
 )
 from constructs import Construct
+
+from aws_solutions_constructs import (
+    aws_lambda_ssmstringparameter as lambdassm
+
+)
 
 class SsmlambdaStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        
+        lambdassm.LambdaToSsmstringparameter(
+            self, 'test-lambda-ssmstringparameter-stack',
+            lambda_function_props=_lambda.FunctionProps(
+                code=_lambda.Code.from_asset('lambda'),
+                runtime=_lambda.Runtime.PYTHON_3_10,
+                handler='index.handler'
+            ),
+            string_parameter_props=ssm.StringParameterProps(
+                string_value="test-string-value")
+        )
 
+        # The code that defines your stack goes here
+        
         # example resource
         # queue = sqs.Queue(
         #     self, "SsmlambdaQueue",
